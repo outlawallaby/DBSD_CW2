be580b5901e6e0d59bf316f47f3cb4f3cbf79d9f
00008487 <vosiqov18.o@gmail.com>
2021-04-15 17:04:35 +0500

    database query

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿USE [USMALL]
+GO
+set language english;
+GO
+
+CREATE TABLE [dbo].[Product]
+(
+    [ProductId] INT NOT NULL IDENTITY,
+    [ProductName] NVARCHAR(70) NOT NULL,
+    [ProductVendor] NVARCHAR(70) NOT NULL,
+    [ProductDescription] Text NOT NULL,
+
+
+
+    [ProductDetailsId] INT NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([ProductId])
+);
