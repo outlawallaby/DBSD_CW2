d65d7a560494281dc5aaedaebcc789d5ce25b16a
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-16 14:41:30 +0500

    Initalize new project

---
+++ USMALL/USMALL/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USMALL.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string JobTitle { get; set; }
+        public string Department { get; set; }
+        public int ReportsTo { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
